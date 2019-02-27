fdeeae0a50b87e40d7850f8b06c81bdffb86ce15
Samuel Colville <S00182360@mail.itsligo.ie>
2019-02-27 16:06:35 +0000

    Initialial setup complete

---
+++ FOOP CA1 - Task Manager/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP_CA1___Task_Manager
+{
+    abstract class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Family:Person
+    {
+        enum Reltaion { Parent, Child, Spouse, Partner, Sibling, Friend, Other}
+
+    }
+
+    class Colleague : Person
+    {
+        enum WorkRelation { Superior, Equal, Subordinate}
+    }
+}
---
+++ FOOP CA1 - Task Manager/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP_CA1___Task_Manager
+{
+    class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public List<string> Labels { get; set; }
+        public Person ShareWith { get; set; }
+        public enum Priority { None, Low, Medium, High}
+
+    }
+
+    class WorkTask : Task
+    {
+
+    }
+}
